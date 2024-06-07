a85c8811bb2b3fc8d0919fcabc2628b790ba5b13
Radwan <radwan.hussein@protonmail.com>
2024-06-07 00:00:50 -0400

    sign in and sign up pages

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,10 +1,9 @@
 import Image from "next/image";
-import { Button } from "@/components/ui/button";
-
+import { UserButton } from "@clerk/nextjs";
 export default function Home() {
   return (
     <>
-    <div>protected page</div>
+    <UserButton afterSignOutUrl="/"/>
     </>
   );
 }
